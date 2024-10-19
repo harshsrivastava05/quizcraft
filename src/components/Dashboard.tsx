dc77fad4c34fd3a5c98bf3d03b17df31e309bf29
Harsh srivastava <harshsrivastava2316@gmail.com>
2024-10-19 17:55:54 +0530

    commit

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,9 @@
+interface PageProps {
+  subscriptionPlan: true | false;
+}
+
+const Dashboard = ({ subscriptionPlan }: PageProps) => {
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,34 @@
+'use client'
 
-const Providers = () => {
+import { trpc } from '@/app/_trpc/client'
+import { absoluteUrl } from '@/lib/utils'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { httpBatchLink } from '@trpc/client'
+import { PropsWithChildren, useState } from 'react'
+
+const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient())
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: absoluteUrl("/api/trpc"),
+        }),
+      ],
+    })
+  )
+
   return (
-    <div>Providers</div>
+    <trpc.Provider
+      client={trpcClient}
+      queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </trpc.Provider>
   )
 }
 
