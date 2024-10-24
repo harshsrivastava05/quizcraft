0633ee0a51a49b4032797010eee089d7e5ca56fa
Harsh srivastava <harshsrivastava2316@gmail.com>
2024-10-24 21:14:47 +0530

    commit

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,5 @@
+const uploadButton = ({ isSubscribed }: { isSubscribed: boolean }) => {
+  return <div>uploadButton</div>;
+};
+
+export default uploadButton;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,9 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { publicProcedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 import { nanoid } from "nanoid";
+import { z } from "zod";
 
 type AuthCallbackResult = {
   success: boolean;
@@ -13,7 +14,7 @@ export const appRouter = router({
     const { getUser } = getKindeServerSession();
     const user = await getUser();
 
-    if (!user.id || !user.email) throw new TRPCError( {code:"UNAUTHORIZED"} );
+    if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
 
     // check if the user is in the database
     const dbUser = await db.user.findFirst({
@@ -38,6 +39,39 @@ export const appRouter = router({
       success: true,
     };
   }),
+
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx;
+
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    });
+  }),
+
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
